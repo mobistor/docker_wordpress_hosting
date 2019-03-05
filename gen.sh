1ec4a316e728aa141f5e884917d5f048ff134277
root <root@vultr.guest>
2019-03-05 05:39:44 +0000

    fix gen.sh vhost to vhosts

--- gen.sh
+++ gen.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 [ -z $1 ] && echo "$0 domain_name" && exit 1
-[ -d ./vhost/$1 ] && echo "$1 already exists!" && exit 1
+[ -d ./vhosts ] || mkdir -p vhosts
+[ -d ./vhosts/$1 ] && echo "$1 already exists!" && exit 1
 
-cp -dpfR ./template ./vhost/$1
+cp -dpfR ./template ./vhosts/$1
 
 
